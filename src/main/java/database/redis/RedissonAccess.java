cb3b71a070e362ae2c3986b3822fa3d0b7b76684
Terrier Li <carronailo@gmail.com>
2017-06-23 13:37:28 +0800

    调整项目mavern依赖库设置
    Signed-off-by: Terrier Li <carronailo@gmail.com>

--- src/main/java/database/redis/RedissonAccess.java
+++ src/main/java/database/redis/RedissonAccess.java
@@ -6,7 +6,9 @@ import org.redisson.api.RedissonClient;
 import org.redisson.codec.FstCodec;
 import org.redisson.config.Config;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 
 /**
  * Created by CarroNailo on 2017/5/15.
@@ -19,26 +21,54 @@ public class RedissonAccess
 		Config redisConfig = new Config();
 		redisConfig.setCodec(new FstCodec()).useSingleServer().setAddress("127.0.0.1:6379").setPassword("1qaz2wsx").setDatabase(1);
 		RedissonClient client = Redisson.create(redisConfig);
+
+		RBucket<database.redis.TestUser> bucket = client.getBucket("user1");
+		TestUser user1 = bucket.get();
+		if (user1 == null)
+		{
+			System.out.println("create new TestUser");
+			user1 = new TestUser();
+			user1.roleID = System.currentTimeMillis();
+			user1.nickName = "carronailo";
+			user1.level = 1;
+			user1.vipLevel = 7;
+			user1.gold = 1000;
+			Calendar c = Calendar.getInstance();
+			c.setTimeInMillis(System.currentTimeMillis());
+			c.set(2017, 5, 15, 19, 00, 00);
+			user1.lastLogin = c.getTime();
+			user1.attributeMap = new HashMap<>();
+			user1.attributeMap.put(TestUser.ATTR_STR, 10.0);
+			user1.attributeMap.put(TestUser.ATTR_DEX, 10.0);
+			user1.attributeMap.put(TestUser.ATTR_INT, 10.0);
+			user1.attributeMap.put(TestUser.ATTR_ATT, 200.0);
+			user1.attributeMap.put(TestUser.ATTR_DEF, 50.0);
+			user1.attributeMap.put(TestUser.ATTR_HP, 2133.0);
+			user1.attributeMap.put(TestUser.ATTR_MP, 73.0);
+			user1.friendList = new ArrayList<>();
+			user1.friendList.add(1234567890L);
+			user1.friendList.add(98764315649852L);
+			user1.friendList.add(87543544966963L);
+			bucket.set(user1);
+		}
+		RBucket strBucket = client.getBucket("user1");
+		Object user1Str = strBucket.get();
+
+		System.out.println(user1);
+		System.out.println(user1Str);
+
+		client.shutdown();
+
 //		List<Object> res = client.getList("mlist");
 //		for(Object o : res)
 //		{
 //			System.out.println(o);
 //		}
-		TestUser user1 = new TestUser();
-		user1.roleID = System.currentTimeMillis();
-		user1.nickName = "carronailo";
-		user1.level = 1;
-		user1.vipLevel = 7;
-		user1.gold = 1000;
-		Calendar c = Calendar.getInstance();
-		c.setTimeInMillis(System.currentTimeMillis());
-		c.set(2017, 5, 15, 19, 00, 00);
-		user1.lastLogin = c.getTime();
 //		RBucket<database.redis.TestUser> bucket = client.getBucket("lijing1");
 //		bucket.set(user1);
-		RBucket<TestUser> bucket = client.getBucket("lijing1");
-		TestUser user = bucket.get();
-		System.out.println(user.toString());
-		client.shutdown();
+//		RBucket<TestUser> bucket = client.getBucket("lijing1");
+//		TestUser user = bucket.get();
+//		System.out.println(user.toString());
+//		client.shutdown();
 	}
 }
--- src/main/java/database/redis/TestUser.java
+++ src/main/java/database/redis/TestUser.java
@@ -2,23 +2,63 @@ package database.redis;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by CarroNailo on 2017/5/15.
  */
+
 public class TestUser implements Serializable
 {
+	public static final int ATTR_STR = 1;
+	public static final int ATTR_DEX = 2;
+	public static final int ATTR_INT = 3;
+	public static final int ATTR_ATT = 4;
+	public static final int ATTR_DEF = 5;
+	public static final int ATTR_HP = 6;
+	public static final int ATTR_MP = 7;
+
 	public long roleID;
 	public String nickName;
 	public int level;
 	public int vipLevel;
 	public int gold;
 	public Date lastLogin;
+	public List<Long> friendList;
+	public Map<Integer, Double> attributeMap;
 
 	@Override
 	public String toString()
 	{
-		return "" + roleID + " " + nickName + " " + level + " " + vipLevel + " " + gold + " " + lastLogin.toString();
+		StringBuilder sb = new StringBuilder();
+		sb.append(roleID);
+		sb.append(" ");
+		sb.append(nickName);
+		sb.append(" ");
+		sb.append(level);
+		sb.append(" ");
+		sb.append(vipLevel);
+		sb.append(" ");
+		sb.append(gold);
+		sb.append(" ");
+		sb.append(lastLogin.toInstant());
+		sb.append(" ");
+		sb.append(" attr:");
+		for(Map.Entry<Integer, Double> entry : attributeMap.entrySet())
+		{
+			sb.append(entry.getKey());
+			sb.append("-");
+			sb.append(entry.getValue());
+			sb.append(",");
+		}
+		sb.append(" friends:");
+		for(Long friend : friendList)
+		{
+			sb.append(friend.longValue());
+			sb.append(",");
+		}
+		return sb.toString();
 	}
 
 }
