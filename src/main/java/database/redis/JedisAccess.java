ba06b005bc86727545aa08e9c49ccd359303b6d6
Terrier Li <carronailo@gmail.com>
2017-09-29 19:06:32 +0800

    1

--- src/main/java/database/redis/JedisAccess.java
+++ src/main/java/database/redis/JedisAccess.java
@@ -1,23 +1,61 @@
 package database.redis;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+import redis.clients.jedis.exceptions.JedisException;
 
 import java.util.List;
 
 public class JedisAccess
 {
+	JedisPool pool = null;
+
+	public JedisAccess()
+	{
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setBlockWhenExhausted(false);
+		config.setMaxTotal(16);
+		config.setTestOnBorrow(true);
+		pool = new JedisPool(config, "127.0.0.1", 6379, Protocol.DEFAULT_TIMEOUT, "1qaz2wsx");
+	}
+
+	public Jedis getJedis()
+	{
+		try
+		{
+			if(pool != null)
+				return pool.getResource();
+		}
+		catch(JedisException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void returnJedis(Jedis jedis)
+	{
+		jedis.close();
+	}
+
+
 	public static void main(String[] args)
 	{
-		Jedis jedis;
-		jedis = new Jedis("127.0.0.1", 6379);
-		//jedis.auth("admin");
+		JedisAccess access = new JedisAccess();
+		Jedis jedis = access.getJedis();
+		if(jedis != null)
+		{
+			String res = jedis.get("lijing");
+			System.out.println(res);
 
-		String res = jedis.get("lijing");
-		System.out.println(res);
+			List<String> listRes = jedis.lrange("mlist", 0, -1);
+			for(String s : listRes)
+				System.out.println(s);
 
-		List<String> listRes = jedis.lrange("mlist", 0, -1);
-		for(String s : listRes)
-			System.out.println(s);
+			jedis.close();
+		}
 	}
 
 }
